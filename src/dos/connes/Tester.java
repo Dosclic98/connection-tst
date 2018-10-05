555109865b801d9dbb5a7cd3d1820ecf18dd0a2f
Davide <davidesavarro98@gmail.com>
2018-10-05 20:27:16 +0200

    NPE solved

--- src/dos/connes/Tester.java
+++ src/dos/connes/Tester.java
@@ -3,17 +3,23 @@ import jbook.util.*;
 
 public class Tester {
 	public static void main(String[] args) {
-		System.out.println("Inserire la valuta iniziale:\n 1) EURO\n"+
-				"2) DOLLARO\n 3) YEN\n 4) DRACMA\n >>");
+		System.out.println("Inserire la valuta iniziale:\n1) EURO\n"+
+				"2) DOLLARO\n3) YEN\n4) DRACMA\n >>");
 		int choice1 = Input.readInt();
-		System.out.println("Inserire la valuta finale:\n 1) EURO\n"+
-				"2) DOLLARO\n 3) YEN\n 4) DRACMA\n >>");
+		System.out.println("Inserire la valuta finale:\n1) EURO\n"+
+				"2) DOLLARO\n3) YEN\n4) DRACMA\n >>");
 		int choice2 = Input.readInt();
 		System.out.print("Inserire il valore da convertire:\n>>");
 		float amt = Input.readFloat();
 		
 		float[] chlist = new float[] {1, 1.5f, 2, 3};
-		Value[] vallist = new Value[4];
+		Value[] vallist = new Value[] 
+				{
+				new Value(),
+				new Value(),
+				new Value(),
+				new Value(),
+				};
 		for(int i=0;i<4;i++)
 		{
 			vallist[i].val = chlist[i];
