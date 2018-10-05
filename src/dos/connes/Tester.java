da27bdf48326a7a6e17c83864b64dd791b54e7ac
Sonotsugipaa <mark.prl.97@gmail.com>
2018-10-05 20:00:07 +0100

    Fixato NPE

--- src/dos/connes/Tester.java
+++ src/dos/connes/Tester.java
@@ -6,18 +6,33 @@ public class Tester {
 		System.out.println("Inserire la valuta iniziale:\n 1) EURO\n"+
 				"2) DOLLARO\n 3) YEN\n 4) DRACMA\n >>");
 		int choice1 = Input.readInt();
+
 		System.out.println("Inserire la valuta finale:\n 1) EURO\n"+
 				"2) DOLLARO\n 3) YEN\n 4) DRACMA\n >>");
 		int choice2 = Input.readInt();
+
 		System.out.print("Inserire il valore da convertire:\n>>");
 		float amt = Input.readFloat();
-		
+
 		float[] chlist = new float[] {1, 1.5f, 2, 3};
+		/* ---- CODICE VECCHIO ----
 		Value[] vallist = new Value[4];
 		for(int i=0;i<4;i++)
 		{
 			vallist[i].val = chlist[i];
 		}
+		-------------------------- */
+		// ---- CODICE FIXATO ----
+		Value[] vallist = new Value[] {
+				new Value(),
+				new Value(),
+				new Value(),
+				new Value()
+		};
+		for(int i=0; i<vallist.length; i+=1)
+			vallist[i].val = chlist[i];
+		// -----------------------
+
 		float res = vallist[choice1-1].conversion(amt, vallist[choice2-1].val);
 		System.out.print(amt + "-->" + res);
 	}
@@ -25,7 +40,7 @@ public class Tester {
 
 class Value {
 	float val;
-	
+
 	public float conversion(float amount, float valext) {
 		float conv = (valext/val)*amount;
 		return conv;
