47adc48c91f60d0d719ff939e4428411786dde3b
Davide <davidesavarro98@gmail.com>
2018-10-05 10:56:01 +0200

    Value read added
    Added value read and print

--- src/dos/connes/Tester.java
+++ src/dos/connes/Tester.java
@@ -1,7 +1,12 @@
 package dos.connes;
+import jbook.util.*;
 
 public class Tester {
 	public static void main(String[] args) {
-		System.out.println("Ciao");
+		System.out.println("Inserire la valuta iniziale:\n 1) EURO\n"+
+				"2) DOLLARO\n 3) YEN\n 4) DRACMA\n >>");
+		float value = Input.readFloat();
+		
+		System.out.println("Il valore inserito è: " + value);
 	}
 }
